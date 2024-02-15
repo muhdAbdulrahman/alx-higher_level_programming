76a5ab623238db27c1816d7f9ce00002cf932b39
muhdAbdulrahman <mabdul.am25@gmail.com>
2024-02-15 13:39:56 -0800

    db

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- script that creates a table i1n the database and add multiples rows
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUE (1, "John", 10);
+INSERT INTO second_table (id, name, score)
+VALUE (2, "Alex", 3);
+INSERT INTO second_table (id, name, score)
+VALUE (3, "Bob", 14);
+INSERT INTO second_table (id, name, score)
+VALUE (4, "George", 8);
